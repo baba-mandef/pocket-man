fe710f02734cd4e38ae6a6b91ec61853d2f39d24
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-12-11 17:41:43 +0100

    - add delete, history & adding urls

--- pocket/manager/urls.py
+++ pocket/manager/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from pocket.manager.views import home
+from pocket.manager.views import (home, add_expense, add_income, del_activity, monthly_incomes, monthly_expense)
 
 urlpatterns = [
 
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('expenses/add', add_expense, name='expense'),
+    path('incomes/add', add_income, name='income'),
+    path('activity/delete/<int:id>', del_activity, name='delete'),
+    path('incomes/history/<int:month>', monthly_incomes, name='incomes_history'),
+    path('expenses/history/<int:month>', monthly_expense, name='expenses_history'),
 
 ]
