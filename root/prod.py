e7a0a83b1b893f1da8c5676e5d91e11d4b7da4d9
Salaou-Deen Paraiso <pariso03henri@gmail.com>
2020-12-11 21:04:59 +0100

    - config

--- root/prod.py
+++ root/prod.py
@@ -1,6 +1,6 @@
 from root.settings import *
 import dj_database_url
-ALLOWED_HOSTS['pocket-man.herokuapp.com']
+ALLOWED_HOSTS = ['pocket-man.herokuapp.com']
 DEBUG = False
 TEMPLATES_DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
