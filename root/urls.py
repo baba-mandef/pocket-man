56e09a2bb6fc8a8fe5577762ec784e1b3f225818
henri-dev <pariso03henri@gmail.com>
2022-03-22 21:28:29 +0000

    - add router urls

--- root/urls.py
+++ root/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from pocket import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('pocket.users.urls')),
     path('', include('pocket.manager.urls')),
+    path('api/v1/', include(router))
 
 ]
