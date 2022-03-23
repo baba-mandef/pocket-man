b425ed04493fa8a64d65618f673eae6992c13aa3
henri-dev <pariso03henri@gmail.com>
2022-03-23 06:31:03 +0000

    - update

--- pocket/manager/viewsets.py
+++ pocket/manager/viewsets.py
@@ -6,10 +6,11 @@ from pocket.manager.models import Activity
 class ActivityViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'patch']
     serializer_class = ActivitySerializer
-    user = request.user
 
     def get_queryset(self):
         
         queryset = Activity.objects.filter(owner=self.request.user.id)
+
+        return queryset
 
 
